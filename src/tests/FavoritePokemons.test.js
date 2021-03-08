e554319432fdd9996c5e507bf4e29c2c4be04c95
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-03-08 10:24:03 -0300

    Requisito 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Tests the "Favorite Pokémons" component', () => {
+  it('Tests if the person does not have favorite Pokémon.', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const message = getByText('No favorite pokemon found');
+    expect(message).toBeInTheDocument();
+  });
+});
