dee09e3846cd21d0247783359efa8a4e7642d32e
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-03-08 10:44:14 -0300

    Requisito 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Tests the "Not Fund" component.', () => {
+  it('Tests if the page contains an h2 with the text "Page requested not found".', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+    const notFoundText = getByText('Page requested not found');
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  it('Tests whether the page shows the image.', () => {
+    const { getAllByRole } = renderWithRouter(<NotFound />);
+    const imageSource = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const image = getAllByRole('img')[1];
+    expect(image.src).toBe(imageSource);
+  });
+});
