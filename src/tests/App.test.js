7a8b3b97b464c58111c9cea3bcee1253ea7570fb
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-03-08 09:24:19 -0300

    Requisito 01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+it('Test', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const home = getByText('Home');
+  const about = getByText('About');
+  const favorite = getByText('Favorite Pokémons');
+  expect(home).toBeInTheDocument();
+  expect(about).toBeInTheDocument();
+  expect(favorite).toBeInTheDocument();
 });
-//
